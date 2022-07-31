38cbd4432272307756cfd067240f2ce5b36465a6
Tong Shen <TongShen2000@gmail.com>
2022-07-31 22:55:01 +1000

    global keywords in non-interactive mode

--- prepare_dataset.jl
+++ prepare_dataset.jl
@@ -4,10 +4,10 @@ save_path = "../preprocessed_data/clean-train-360/"
 batch_size = 500
 
 mel_folders = readdir(source_path)
-global file_count = 0
-global batch_count = 0
+file_count = 0
+batch_count = 0
 #lengths = []
-global all_mels = []
+all_mels = []
 for folder in mel_folders
     sub_folders = readdir(string(source_path, folder))
     for sub_folder in sub_folders
@@ -17,23 +17,23 @@ for folder in mel_folders
             log_mel = load(file_path)["log_mel"]
             #length = load(file_path)["length"]
             if file_count == 0
-                all_mels = log_mel
+                global all_mels = log_mel
                 #append!(lengths, length)
-                file_count += 1
+                global file_count += 1
                 continue
             elseif file_count == batch_size
                 println("Saving batch ", batch_count)
                 save_name = save_path * "batch_$batch_count.jld"
                 save(save_name, "all_mels", all_mels)
                 sleep(5) # Allow time to save propaply 
-                all_mels = []
-                all_mels = log_mel
-                batch_count += 1
-                file_count = 1
+                global all_mels = []
+                global all_mels = log_mel
+                global batch_count += 1
+                global file_count = 1
             else
-                all_mels = cat(all_mels, log_mel, dims=3)
+                global all_mels = cat(all_mels, log_mel, dims=3)
                 #append!(lengths, length)
-                file_count += 1 
+                global file_count += 1 
             end
         end
     end
@@ -46,6 +46,6 @@ end
 
 batch_file_list = []
 for batch = 0:batch_count
-    batch_file_list = cat(batch_file_list, "batch_$batch.jld", dims=1)
+    global batch_file_list = cat(batch_file_list, "batch_$batch.jld", dims=1)
 end
 save(save_path * "batch_file_list.jld", "file_list", batch_file_list)
