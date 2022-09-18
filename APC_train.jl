9ea9eeb04d0f6f8ceca79f4c502ec3083b2945b3
Tong Shen <TongShen2000@gmail.com>
2022-09-19 01:31:55 +1000

    ready for submiting job

--- APC_train.jl
+++ APC_train.jl
@@ -65,7 +65,7 @@ function train()
     
     file_list = load(dataset_path * "batch_file_list.jld")["file_list"]
     validation = getdata(dataset_path * "batch_0.jld")
-    for epoch = 1:2
+    for epoch = 1:1
         for file_name in file_list
             file_path = dataset_path * file_name
             if file_name == "batch_0.jld"
@@ -74,7 +74,7 @@ function train()
                 batch_x = getdata(file_path)
                 println(size(batch_x))
                 println("training using $file_name")
-                train_loader = Flux.Data.DataLoader(batch_x, batchsize=250, shuffle=true)
+                train_loader = Flux.Data.DataLoader(batch_x, batchsize=50, shuffle=true)
                 Flux.train!(loss, Flux.params(APC, post_net), train_loader, ADAM(0.001))#, cb = Flux.throttle(evalcb, 30))
                 println("validation loss:\n", loss(validation))
             end
