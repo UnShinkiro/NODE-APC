035eb117209bb3d1e8a32a71bed48d26c3850703
Tong Shen <TongShen2000@gmail.com>
2022-09-28 15:31:45 +1000

    copying apc expert.py

--- s3prl/expert.py
+++ s3prl/expert.py
@@ -1,61 +1,40 @@
-from collections import OrderedDict
-from typing import List, Union, Dict
+import torch
+from torch.nn.utils.rnn import pad_packed_sequence, pad_sequence
 
-import torch.nn as nn
-from torch import Tensor
-from torch.nn.utils.rnn import pad_sequence
-
+from ..interfaces import UpstreamBase
 from .audio import create_transform
 
-HIDDEN_DIM = 8
 
-
-class UpstreamExpert(nn.Module):
-    def __init__(self, ckpt: str = None, model_config: str = None, **kwargs):
-        """
-        Args:
-            ckpt:
-                The checkpoint path for loading your pretrained weights.
-                Can be assigned by the -k option in run_downstream.py
-
-            model_config:
-                The config path for constructing your model.
-                Might not needed if you also save that in your checkpoint file.
-                Can be assigned by the -g option in run_downstream.py
-        """
+class UpstreamExpert(UpstreamBase):
+    def __init__(self, ckpt, **kwargs):
         super().__init__(**kwargs)
 
         ckpt = torch.load(ckpt, map_location="cpu")
         config = ckpt["config"]
-        self.name = "[Example UpstreamExpert]"
+
         self.preprocessor, feat_dim = create_transform(config["data"]["audio"])
+        self.model = APC(feat_dim, **config["model"]["paras"])
+        self.model.load_state_dict(ckpt["model"])
 
-        print(
-            f"{self.name} - You can use model_config to construct your customized model: {model_config}"
-        )
-        print(f"{self.name} - You can use ckpt to load your pretrained weights: {ckpt}")
-        print(
-            f"{self.name} - If you store the pretrained weights and model config in a single file, "
-            "you can just choose one argument (ckpt or model_config) to pass. It's up to you!"
-        )
-
-        # The model needs to be a nn.Module for finetuning, not required for representation extraction
-        self.model1 = nn.Linear(1, HIDDEN_DIM)
-        self.model2 = nn.Linear(HIDDEN_DIM, HIDDEN_DIM)
+        if len(self.hooks) == 0:
+            self.add_hook(
+                "self.model.rnn_layers[1]",
+                lambda input, output: pad_packed_sequence(input[0], batch_first=True)[
+                    0
+                ],
+            )
+            self.add_hook(
+                "self.model.rnn_layers[2]",
+                lambda input, output: pad_packed_sequence(input[0], batch_first=True)[
+                    0
+                ],
+            )
+            self.add_hook("self.model", lambda input, output: output[1])
 
     def get_downsample_rates(self, key: str) -> int:
-        """
-        Since we do not do any downsampling in this example upstream
-        All keys' corresponding representations have downsample rate of 1
-        """
-        return 1
+        return 160
 
-    def forward(self, wavs: List[Tensor]) -> Dict[str, Union[Tensor, List[Tensor]]]:
-        """
-        When the returning Dict contains the List with more than one Tensor,
-        those Tensors should be in the same shape to train a weighted-sum on them.
-        """
-
+    def forward(self, wavs):
         features = [self.preprocessor(wav.unsqueeze(0)) for wav in wavs]
         feat_lengths = [len(feat) for feat in features]
 
