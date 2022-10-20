31ad1a2bdb40b0a786f104de1286ab3e19d7d2cb
Tong Shen <TongShen2000@gmail.com>
2022-10-20 13:18:18 +1100

    update APC

--- APC_train.jl
+++ APC_train.jl
@@ -1,16 +1,14 @@
 # Starting Code on Julia version of APC
 using Pkg
-Pkg.activate(".")
+Pkg.activate("/home/z5195063/master/NODE-APC")
 using Flux
 using Flux.Losses: mae
 using CUDA
 using JLD
 using BSON: @save
 using IterTools: ncycle 
-batch_size = 50
-max_len = 800
 feature_size = 512
-dataset_path = "../preprocessed_data/clean-train-360/"
+dataset_path = "train-clean-360-jld"
 
 function build_APC_model(input_dimension)
     return Chain(
@@ -29,9 +27,13 @@ function build_postnet(output_dimension)
 end
 
 
-function getdata(batch_file_path)
-    data = load(batch_file_path)["all_mels"]
-    return [[data[:,frame,idx] for frame=1:Int(data[1,end,idx])] for idx=1:size(data)[3]]
+function getdata(file_path)
+    data = load(file_path)["log_mel"]
+    if (size(data)[1]) > 1200
+        return data[1:1200]
+    else
+        return data
+    end
 end
 
 
@@ -39,18 +41,15 @@ function train()
     APC = build_APC_model(80) |> gpu
     post_net = build_postnet(80) |> gpu
     
-    function loss(batch_data)
-        total_loss = 0
-        for file in batch_data
-            input = file[1:end-1] |> gpu
-            output = file[end] |> gpu
-            Flux.reset!(APC)
-            Flux.reset!(post_net)
-            features = APC.(input) |> gpu
-            prediction = post_net.(features)[end] |> gpu
-            total_loss += sum(abs.(prediction .- output))
-        end
-        println("batch size: ",size(batch_data), "\tloss:", total_loss)
+    function loss(file)
+        input = file[1:end-1] |> gpu
+        output = file[end] |> gpu
+        Flux.reset!(APC)
+        Flux.reset!(post_net)
+        features = APC.(input) |> gpu
+        prediction = post_net.(features)[end] |>gpu
+        total_loss = sum(abs.(prediction .- output))/size(file)[1]
+        println("batch size: ",size(file), "\tloss:", total_loss)
         return total_loss
     end
     
@@ -58,20 +57,19 @@ function train()
     #tx, tl = (batch_x[:,:,5], batch_x[:,max_len+1,5])
     #evalcb = () -> @show println("Next epoch")
     
-    file_list = load(dataset_path * "batch_file_list.jld")["file_list"]
-    validation = getdata(dataset_path * "batch_0.jld")
+    file_list = load(dataset_path * "fileList.jld")["fileList"]
+    file_count = 0
     for epoch = 1:1
         for file_name in file_list
+            file_count += 1
             file_path = dataset_path * file_name
             if file_name == "batch_0.jld"
                 continue
             else
                 batch_x = getdata(file_path)
-                println(size(batch_x))
-                println("training using $file_name")
-                train_loader = Flux.Data.DataLoader(batch_x, batchsize=50, shuffle=true)
+                println("training using $file_name $file_count")
+                train_loader = Flux.Data.DataLoader(batch_x, batchsize=1200, shuffle=false)
                 Flux.train!(loss, Flux.params(APC, post_net), train_loader, ADAM(0.0001))#, cb = Flux.throttle(evalcb, 30))
-                println("validation loss:\n", loss(validation))
             end
         end
     end
