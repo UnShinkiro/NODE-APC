e99293ef763d38b6e1e0cfe1d85aca4882402cc6
Tong Shen <TongShen2000@gmail.com>
2022-10-20 13:26:09 +1100

    getting fileList

--- save_to_jld.py
+++ save_to_jld.py
@@ -10,11 +10,12 @@ for line in file:
     data = line.strip().split()
     if len(data) == 1:
         if data[0] == '.':  # end of the current utterance
-            Main.filename = f'{filepath}/{utt_id}.jld'
-            Main.data = log_mel
-            Main.eval('data = Float32.(data)')
-            Main.eval('data = [data[frame,:] for frame=1:size(data)[1]]')
-            Main.eval('save(filename, "log_mel", data)')
+            #Main.filename = f'{filepath}/{utt_id}.jld'
+            #Main.data = log_mel
+            #Main.eval('data = Float32.(data)')
+            #Main.eval('data = [data[frame,:] for frame=1:size(data)[1]]')
+            #Main.eval('save(filename, "log_mel", data)')
+            pass
 
         else: # here starts a new utterance
             utt_id = data[0]
@@ -23,7 +24,8 @@ for line in file:
             file_list.append(f'{utt_id}.jld')
 
     else:
-        log_mel.append([float(i) for i in data])
+        #log_mel.append([float(i) for i in data])
+        pass
 
 Main.fileList = file_list
 Main.eval('save("train-clean-360-jld/fileList.jld", "fileList", fileList)')
