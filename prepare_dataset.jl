344bc02e6fc698a29b1feadd914875f518271d3e
Tong Shen <TongShen2000@gmail.com>
2022-11-07 21:31:33 +1100

    .

--- prepare_dataset.jl
+++ prepare_dataset.jl
@@ -1,52 +1,29 @@
 using JLD
-source_path = "../log_mels/"
+source_path = "../train-clean-360-jld/"
 save_path = "../preprocessed_data/clean-train-360/"
-batch_size = 500
+batch_size = 32
 
-mel_folders = readdir(source_path)
-file_count = 0
-batch_count = 0
-#lengths = []
-all_mels = []
-for folder in mel_folders
-    println("Handling folder ", folder)
-    sub_folders = readdir(string(source_path, folder))
-    for sub_folder in sub_folders
-        files = readdir(string(source_path, folder, "/", sub_folder))
-        for file in files
-            file_path = string(string(source_path, folder, "/", sub_folder, "/", file))
-            log_mel = load(file_path)["log_mel"]
-            #length = load(file_path)["length"]
-            if file_count == 0
-                global all_mels = log_mel
-                #append!(lengths, length)
-                global file_count += 1
-                continue
-            elseif file_count == batch_size
-                println("Saving batch ", batch_count)
-                save_name = save_path * "batch_$batch_count.jld"
-                save(save_name, "all_mels", all_mels)
-                sleep(5) # Allow time to save propaply 
-                global all_mels = []
-                global all_mels = log_mel
-                global batch_count += 1
-                global file_count = 1
-            else
-                global all_mels = cat(all_mels, log_mel, dims=3)
-                #append!(lengths, length)
-                global file_count += 1 
-            end
+files = readdir(source_path)
+file_count = 1
+batch_count = 1
+for file in files
+    println("Handling file ", file)
+    log_mel = load(source_path * file)["log_mel"]
+    if file_count == 1
+        batch_mel = log_mel
+    else
+        if file_count == batch_size
+            file_count = 1
+            println("Saving batch ", batch_count)
+            save_name = save_path * "batch_$batch_count.jld"
+            save(save_name, "batch_mel", batch_mel)
+            batch_count += 1
         end
+        batch_mel = [batch_mel log_mel]
     end
 end
 if file_count < batch_size
     println("Saving batch ", batch_count)
     save_name = save_path * "batch_$batch_count.jld"
-    save(save_name, "all_mels", all_mels)
+    save(save_name, "batch_mel", batch_mel)
 end
-
-batch_file_list = []
-for batch = 0:batch_count
-    global batch_file_list = cat(batch_file_list, "batch_$batch.jld", dims=1)
-end
-save(save_path * "batch_file_list.jld", "file_list", batch_file_list)
