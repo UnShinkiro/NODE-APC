2b7228b1324ada84c2aa2fbdf28b016210aa3115
Tong Shen <TongShen2000@gmail.com>
2022-11-09 22:38:39 +1100

    adding Gadi pbs job

---
+++ Gadi/downscale_evaluation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+#PBS -l ncpus=12
+#PBS -l ngpus=1
+#PBS -l mem=32GB
+#PBS -l jobfs=200GB
+#PBS -q gpuvolta
+#PBS -P wa66
+#PBS -l walltime=60:00:00
+#PBS -l storage=gdata/wa66
+#PBS -l wd
+
+module load julia/1.7.1
+module load cuda/10.1
+cd $PBS_JOBFS
+cp -r $HOME/NODE-APC .
+cp /g/data/wa66/Tong/devNODEmodel.bson .
+cp /g/data/wa66/Tong/train-clean-360-jld.tar.gz .
+tar -xf train-clean-360-jld.tar.gz
+cd NODE-APC
+julia evaluate.jl > $HOME/devNODEmodel.txt
