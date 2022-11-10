4de30924969b99b5ffb033f15ae5e57897c147f1
Tong Shen <ts7017@gadi-gpu-v100-0071.gadi.nci.org.au>
2022-11-10 15:10:06 +1100

    Adding new log

--- Gadi/downscale_evaluation.sh
+++ Gadi/downscale_evaluation.sh
@@ -6,7 +6,7 @@
 #PBS -l jobfs=200GB
 #PBS -q gpuvolta
 #PBS -P wa66
-#PBS -l walltime=48:00:00
+#PBS -l walltime=2:00:00
 #PBS -l storage=gdata/wa66
 #PBS -l wd
 
@@ -14,8 +14,8 @@ module load julia/1.7.1
 module load cuda/10.1
 cd $PBS_JOBFS
 cp -r $HOME/NODE-APC .
-cp /g/data/wa66/Tong/devNODEmodel.bson .
+cp /g/data/wa66/Tong/devDownscaledAPCmodel.bson .
 cp /g/data/wa66/Tong/train-clean-360-jld.tar.gz .
 tar -xf train-clean-360-jld.tar.gz
 cd NODE-APC
-julia evaluate.jl a > $HOME/devNODEmodel.txt
+julia evaluate.jl downscale > $HOME/devDownscaledmodel.txt
